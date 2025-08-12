1c8bb633521efb3782463b4e7c241f8d1d45a1c7
Dachi Kvirikadze <dachikvirikadze01@gmail.com>
2025-08-12 22:38:11 +0400

    asdasdasdasd

--- src/Components/Song.jsx
+++ src/Components/Song.jsx
@@ -8,7 +8,7 @@ const Sunflower = () => {
           <div className="bg-half left"></div>
           <div className="bg-half right"></div>
             <div className='iframe-wrapper'>
-                <iframe class="yt-link" width="360" height="215" src="https://www.youtube.com/embed/_mDxcDjg9P4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className="yt-link" width="360" height="215" src="https://www.youtube.com/embed/_mDxcDjg9P4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </div>
     </>
